472bce6d37d16bf26d799eb7996f3251e7581e78
riancmd <rian.dssantos@gmail.com>
2025-11-22 18:52:12 -0300

    ✨ feat: acrescenta o core do algoritmo PoW e atualiza criação de bloco

--- internal/infrastructure/blockchain/block.go
+++ internal/infrastructure/blockchain/block.go
@@ -1,12 +1,9 @@
 package blockchain
 
 import (
-	"bytes"
-	"crypto/sha256"
 	"encoding/json"
 	"log/slog"
 	"pbl-2-redes/internal/models"
-	"strconv"
 	"time"
 )
 
@@ -15,35 +12,27 @@ type Block struct {
 	Hash         []byte // contém o hash do bloco
 	Transactions []byte // contém as transações
 	PreviousHash []byte // contém o hash do bloco anterior
-	Target       int    // valor target do algoritmo PoW
 	Nonce        int    // nonce usado para o hash
 }
 
 func NewBlock(p []byte, t []models.Transaction) *Block {
-	// serializa struct de transações
+	// Serializa struct de transações
 	data, err := json.Marshal(t)
 	if err != nil {
 		slog.Error(err.Error())
 		return nil
 	}
 
-	block := &Block{Timestamp: time.Now().Unix(), Transactions: data, PreviousHash: p, Target: 1, Nonce: 10000}
-	block.SetHash()
+	// Cria primeira versão do bloco, sem hash ainda
+	block := &Block{Timestamp: time.Now().Unix(), Transactions: data, PreviousHash: p}
+
+	// Cria o PoW
+	pow := NewProofOfWork(block)
+
+	// Guarda o nonce que for encontrado e joga no bloco
+	nonce, hash := pow.Run()
+	block.Hash = hash[:]
+	block.Nonce = nonce
 
 	return block
-}
-
-/*
-func ComputeHash(b *models.Block) {
-	data := b.Data
-	hash := sha256.Sum256([]byte(data))
-	b.Hash = hex.EncodeToString(hash[:])
-}*/
-
-func (b *Block) SetHash() {
-	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
-	headers := bytes.Join([][]byte{b.PreviousHash, b.Transactions, timestamp}, []byte{})
-	hash := sha256.Sum256(headers)
-
-	b.Hash = hash[:]
 }
--- internal/infrastructure/blockchain/pow.go
+++ internal/infrastructure/blockchain/pow.go
@@ -1,8 +1,82 @@
 package blockchain
 
-import "math/big"
+import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/binary"
+	"fmt"
+	"log/slog"
+	"math"
+	"math/big"
+)
+
+const targetBits = 24
 
 type ProofOfWork struct {
 	block  *Block
 	target *big.Int
+}
+
+// gera novo algoritmo de PoW para blcoo especificado
+func NewProofOfWork(b *Block) *ProofOfWork {
+	target := big.NewInt(1)
+	target.Lsh(target, uint(256-targetBits))
+
+	pow := &ProofOfWork{b, target}
+
+	return pow
+}
+
+// prepara os dados para o PoW
+func (pow *ProofOfWork) prepareData(nonce int) []byte {
+	data := bytes.Join(
+		[][]byte{
+			pow.block.PreviousHash,
+			pow.block.Transactions,
+			IntToHex(pow.block.Timestamp),
+			IntToHex(int64(targetBits)),
+			IntToHex(int64(nonce)),
+		},
+		[]byte{},
+	)
+
+	return data
+}
+
+// função core do PoW
+func (pow *ProofOfWork) Run() (int, []bytes) {
+	var hashInt big.Int
+	var hash [32]byte
+	nonce := 0
+
+	fmt.Printf("Minerando novo bloco.\n")
+
+	// coloca que o máximo que o nonce pode chegar é o valor máximo suportado de um inteiro de 64 bits
+	for nonce < math.MaxInt64 {
+		data := pow.prepareData(nonce)
+		hash = sha256.Sum256(data)
+		fmt.Printf("\r%x", hash)
+		hashInt.SetBytes(hash[:])
+
+		if hashInt.Cmp(pow.target) == -1 {
+			break
+		} else {
+			nonce++
+		}
+	}
+	fmt.Print("\n")
+	return nonce, hash[:]
+}
+
+// converte para hexadecimal de inteiro
+func IntToHex(data int64) []byte {
+	buff := new(bytes.Buffer)
+
+	err := binary.Write(buff, binary.BigEndian, data)
+	if err != nil {
+		slog.Error(err.Error())
+		return []byte{}
+	}
+
+	return buff.Bytes()
 }
